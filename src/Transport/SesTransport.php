e316d04d5d07eb964e485b163b1f1ddc6cceccc6
bogdan <bogdan@thesoftwarefarm.co.uk>
2020-10-28 11:25:02 +0200

    ses transport redone

--- src/Transport/SesTransport.php
+++ src/Transport/SesTransport.php
@@ -2,8 +2,10 @@
 
 namespace TsfCorp\Email\Transport;
 
+use Aws\Ses\Exception\SesException;
 use Aws\Ses\SesClient;
-use Throwable;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\Mime\Email;
 use TsfCorp\Email\Models\EmailModel;
 
 class SesTransport extends Transport
@@ -30,34 +32,47 @@ class SesTransport extends Transport
     {
         try
         {
-            $response = $this->ses->sendEmail([
-                'Source' => $email->prepareFromAddress(),
-                'Destination' => [
-                    'ToAddresses' => $email->prepareToAddress() ?? [],
-                    'CcAddresses' => $email->prepareCcAddress() ?? [],
-                    'BccAddresses' => $email->prepareBccAddress() ?? [],
-                ],
-                'Message' => [
-                    'Body' => [
-                        'Html' => [
-                            'Data' => $email->body,
-                        ],
-                        'Text' => [
-                            'Data' => 'To view the message, please use an HTML compatible email viewer',
-                        ],
-                    ],
-                    'Subject' => [
-                        'Data' => $email->subject,
-                    ],
-                ],
+            $from = $email->decodeRecipient($email->from);
+
+            $to = array_map(function ($recipient) {
+                return new Address($recipient->email, $recipient->name ? $recipient->name : '');
+            }, $email->decodeRecipient($email->to));
+
+            $cc = array_map(function ($recipient) {
+                return new Address($recipient->email, $recipient->name ? $recipient->name : '');
+            }, $email->decodeRecipient($email->cc));
+
+            $bcc = array_map(function ($recipient) {
+                return new Address($recipient->email, $recipient->name ? $recipient->name : '');
+            }, $email->decodeRecipient($email->bcc));
+
+            $attachments = json_decode($email->attachments, true);
+
+            $symfony_email = (new Email())
+                ->from(new Address($from->email, $from->name ? $from->name : ''))
+                ->to(...$to)
+                ->cc(...$cc)
+                ->bcc(...$bcc)
+                ->subject($email->subject)
+                ->text('To view the message, please use an HTML compatible email viewer')
+                ->html($email->body);
+
+            if (!empty($attachments)) {
+                foreach ($attachments as $attachment_path) {
+                    $symfony_email->attachFromPath($attachment_path);
+                }
+            }
+
+            $response = $this->ses->sendRawEmail([
+                'RawMessage' => [
+                    'Data' => $symfony_email
+                ]
             ]);
 
             $this->remote_identifier = $response->get('MessageId');
             $this->message = 'Queued.';
-        }
-        catch (Throwable $t)
-        {
-            throw $t;
+        } catch (SesException $error) {
+            throw $error->getAwsErrorMessage();
         }
     }
 }
