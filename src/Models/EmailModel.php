7795724fcf4d479dea447b2fe5efd842569f0c17
bogdan <bogdan@thesoftwarefarm.co.uk>
2020-10-28 12:54:52 +0200

    code cleanup

--- src/Models/EmailModel.php
+++ src/Models/EmailModel.php
@@ -3,9 +3,7 @@
 namespace TsfCorp\Email\Models;
 
 use Carbon\Carbon;
-use Illuminate\Database\Connection;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use TsfCorp\Email\Jobs\EmailJob;
 
 /**
