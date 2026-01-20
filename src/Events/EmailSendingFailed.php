0f91d53f3b7cab6857c909b8f9422c30e840aa0d
Ionut Antohi <ionut.antohi@gmail.com>
2026-01-20 15:45:03 +0200

    remove docs

--- src/Events/EmailSendingFailed.php
+++ src/Events/EmailSendingFailed.php
@@ -7,19 +7,9 @@ use TsfCorp\Email\Models\EmailModel;
 
 class EmailSendingFailed
 {
-    /**
-     * @var \TsfCorp\Email\Models\EmailModel
-     */
-    public $email;
-    /**
-     * @var \Throwable|null
-     */
-    public $exception;
+    public EmailModel $email;
+    public ?Throwable $exception;
 
-    /**
-     * @param \TsfCorp\Email\Models\EmailModel $email
-     * @param \Throwable|null $exception
-     */
     public function __construct(EmailModel $email, ?Throwable $exception = null)
     {
         $this->email = $email;
--- src/Events/EmailSendingSucceeded.php
+++ src/Events/EmailSendingSucceeded.php
@@ -6,15 +6,8 @@ use TsfCorp\Email\Models\EmailModel;
 
 class EmailSendingSucceeded
 {
-    /**
-     * @var \TsfCorp\Email\Models\EmailModel
-     */
-    public $email;
+    public EmailModel $email;
 
-    /**
-     * EmailSent constructor.
-     * @param \TsfCorp\Email\Models\EmailModel $email
-     */
     public function __construct(EmailModel $email)
     {
         $this->email = $email;
